097bb2d3da5760853a3f6ca7d1d771720bf1bdcb
Bhandari <vaibhav.bhandari@capgemini.com>
2019-12-19 14:53:41 +0530

    message

--- app.js
+++ app.js
@@ -171,8 +171,9 @@ app.post('/snow', function (req, response) {
                 slack.send({				  
                     channel: 'gisdevelopservicenow',
                     text:  'Ticket Number '+res[i].number + " status is " +res[i].incident_state 
-                });  
-                response.write(JSON.stringify({ "fulfillmentText": "Ticket number: " + res[i].number + " and urgency " + res[i].urgency +"/ n"}));
+                });
+                response.write(resp);
+                //response.write(JSON.stringify({ "fulfillmentText": "Ticket number: " + res[i].number + " and urgency " + res[i].urgency +"/ n"}));
                 response.end();
                // response.end();
                 //     res.send(resp);
